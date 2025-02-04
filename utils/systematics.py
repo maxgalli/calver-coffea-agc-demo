0d87dcf11cbbedc7de66e7c59fd56f1b85eb09d1
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2025-02-04 13:41:57 +0000

    development

--- utils/systematics.py
+++ utils/systematics.py
@@ -1,20 +1,5 @@
 import awkward
 import numpy
-import dask.array as da
-import dask_awkward as dak
-
-# functions creating systematic variations
-#def jet_pt_resolution(pt):
-#    # normal distribution with 5% variations, shape matches jets
-#    counts = ak.num(pt)
-#    pt_flat = ak.flatten(pt)
-#    # get size of flattened array
-#    size = dak.num(pt_flat)
-#    print(size)
-#    #resolution_variation = np.random.normal(np.ones_like(pt_flat), 0.05)
-#    #return ak.unflatten(resolution_variation, counts)
-#    resolution_variation = da.random.normal(1, 0.05, size=pt_flat.size)
-#    return ak.unflatten(resolution_variation, counts)
 
 def rand_gauss(item):
     seeds = (
